7de3e82aa62e58065a9ba763f0fbe1ff20517945
miguelsaliba <36637899+miguelsaliba@users.noreply.github.com>
2020-04-10 18:01:55 +0300

    done

--- src/calculator.java
+++ src/calculator.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class calculator {
     public static void main(String[] args) {
-        simplify("2*3*((12+7)^2+4)-3/2");
+        simplify("10^(2+0)");
     }
 
     private static String[] removeElement(String[] theArray, int firstIndex, int lastIndex){
@@ -29,76 +29,112 @@ public class calculator {
         DecimalFormat format = new DecimalFormat("#.#");
         format.setDecimalSeparatorAlwaysShown(false);
 
-        String[] temp = message.split("(?<=[()+\\-*/^])|(?=[()+\\-*/^])");
-        System.out.println(Arrays.asList(temp).toString().substring(1).replaceFirst("]", "").replace(", ", ""));
+        StringBuilder builder = new StringBuilder(message);
+
+        //String[] temp = message.split("(?<=[()+\\-*/^])|(?=[()+\\-*/^])");
+        System.out.println(builder);
 
         int index1 = -1;
-        for (int i = 0; i < temp.length; i++) {
-            if (temp[i].equals("(")) {
+        for (int i = 0; i < builder.length(); i++) {
+            if (builder.charAt(i) == '(') {
                 index1 = i;
-            } else if (temp[i].equals(")")) {
+            } else if (builder.charAt(i) == ')') {
                 if (index1 == -1) {
                     return "error: incorrect bracket placement";
                 }
-                String[] insideParentheses;
+                StringBuilder insideParentheses;
                 if (index1+1 == i){
-                    insideParentheses = Arrays.copyOfRange(temp, index1, i);
+                    insideParentheses = new StringBuilder(builder.substring(index1, i));
                 } else {
-                    insideParentheses = Arrays.copyOfRange(temp, index1 + 1, i);
+                    insideParentheses = new StringBuilder(builder.substring(index1+1, i));
                 }
-                String num = simplify(String.join("", Arrays.asList(insideParentheses)));
-                temp[i] = num;
-                temp = removeElement(temp, index1, i-1);
-                i = 0;
-                System.out.println(Arrays.asList(temp).toString().substring(1).replaceFirst("]", "").replace(", ", ""));
-            }
-        }
-
-        for (int i = 0; i < temp.length; i++) {
-            if (temp[i].equals("^")) {
-                temp[i+1] = String.valueOf( format.format( Math.pow( Double.parseDouble(temp[i-1]),  Double.parseDouble(temp[i+1]) )));
-                temp = removeElement(temp, i-1, i);
-                i -= 2;
-                System.out.println(Arrays.asList(temp).toString().substring(1).replaceFirst("]", "").replace(", ", ""));
-
-            }
-        }
-
-        for (int i = 0; i < temp.length; i++) {
-            if (temp[i].equals("*")) {
-                temp[i+1] = String.valueOf( format.format( Double.parseDouble(temp[i-1]) *  Double.parseDouble(temp[i+1]) ));
-                temp = removeElement(temp, i-1, i);
-                i -= 2;
-                System.out.println(Arrays.asList(temp).toString().substring(1).replaceFirst("]", "").replace(", ", ""));
-
-            } else if (temp[i].equals("/")){
-                if (temp[i + 1].equals("0")) {
+                String num = simplify(insideParentheses.toString());
+                if (num.equals("error: dividing by zero")){
                     return "error: dividing by zero";
                 }
-                temp[i+1] = String.valueOf( format.format( Double.parseDouble(temp[i-1]) /  Double.parseDouble(temp[i+1]) ));
-                temp = removeElement(temp, i-1, i);
-                i -= 2;
-                System.out.println(Arrays.asList(temp).toString().substring(1).replaceFirst("]", "").replace(", ", ""));
-
+                builder.replace(index1, i+1, num);
+                i = 0;
+                System.out.println(builder);
             }
         }
 
-        for (int i = 0; i < temp.length; i++) {
-            if (temp[i].equals("+")) {
-                temp[i+1] = String.valueOf( format.format( Double.parseDouble(temp[i-1]) +  Double.parseDouble(temp[i+1]) ));
-                temp = removeElement(temp, i-1, i);
-                i -= 2;
-                System.out.println(Arrays.asList(temp).toString().substring(1).replaceFirst("]", "").replace(", ", ""));
+        int index = builder.indexOf("^");
+        while (index != -1) {
+            int before = operatorBefore(builder, index);
+            int after = operatorAfter(builder, index);
 
-            } else if (temp[i].equals("-")){
-                temp[i+1] = String.valueOf( format.format( Double.parseDouble(temp[i-1]) -  Double.parseDouble(temp[i+1]) ));
-                temp = removeElement(temp, i-1, i);
-                i -= 2;
-                System.out.println(Arrays.asList(temp).toString().substring(1).replaceFirst("]", "").replace(", ", ""));
-
-            }
+            builder.replace(before, after, format.format( Math.pow( Double.parseDouble(builder.substring(before, index)),  Double.parseDouble(builder.substring(index+1, after)) )));
+            System.out.println(builder);
+            index = builder.indexOf("^");
         }
 
-        return temp[0];
+        index = Math.max(builder.indexOf("*"), builder.indexOf("/"));
+        while (index != -1) {
+            if (!solve(builder, index, format)){
+                return "error: dividing by zero";
+            }
+            System.out.println(builder);
+            index = Math.max(builder.indexOf("*"), builder.indexOf("/"));
+        }
+
+        index = Math.max(builder.indexOf("+"), builder.indexOf("-"));
+        while (index != -1) {
+            solve(builder, index, format);
+            System.out.println(builder);
+            index = Math.max(builder.indexOf("+"), builder.indexOf("-"));
+        }
+
+        return builder.toString();
+    }
+
+    public static boolean solve(StringBuilder builder, int index, DecimalFormat format){
+        int before = operatorBefore(builder, index);
+        int after = operatorAfter(builder, index);
+
+        double num1 = Double.parseDouble(builder.substring(before, index));
+        double num2 = Double.parseDouble(builder.substring(index + 1, after));
+
+        switch (builder.charAt(index)) {
+            case '^':
+                builder.replace(before, after, format.format(Math.pow(num1, num2)));
+                break;
+            case '*':
+                builder.replace(before, after, format.format(num1 * num2));
+                break;
+            case '/':
+                if (num2 == 0){
+                    return false;
+                }
+                builder.replace(before, after, format.format(num1 / num2));
+                break;
+            case '+':
+                builder.replace(before, after, format.format(num1 + num2));
+                break;
+            case '-':
+                builder.replace(before, after, format.format(num1 - num2));
+                break;
+            default:
+                System.out.println("idk how u got here tbh");
+
+        }
+        return true;
+    }
+
+    public static int operatorBefore(StringBuilder str, int index) {
+        for (int i = index-1; i > 0; i--){
+            if (!Character.isDigit(str.charAt(i)) && str.charAt(i) != '.'){
+                return i+1;
+            }
+        }
+        return 0;
+    }
+
+    public static int operatorAfter(StringBuilder str, int index) {
+        for (int i = index+1; i < str.length(); i++){
+            if (!Character.isDigit(str.charAt(i)) && str.charAt(i) != '.'){
+                return i-1;
+            }
+        }
+        return str.length();
     }
 }
